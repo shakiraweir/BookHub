f6fe94572b8d82b689d7a474251db33280859869
Shakira Weir <shakira.weir87@gmail.com>
2019-02-01 00:58:16 -0500

    setting up for Heroku production

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/bookapp");
+
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect("mongodb://localhost/bookapp");
+  }
 mongoose.Promise = Promise;
 module.exports = mongoose;
